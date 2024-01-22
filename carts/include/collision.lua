438a25c785697a74e656f6082ca4304e2c2ac7bc
Rosthouse <rosthouse@gmail.com>
2024-01-22 09:22:17 +0100

    Adding title screen Drawing sprites Adding powerups

--- carts/include/collision.lua
+++ carts/include/collision.lua
@@ -1,20 +1,20 @@
 
 
 function rbox(x, y, w, h)
-    return {x1= x, y1= y, x2=x+w, y2=y+h}
+    return {x0=x, y0= y, x1=x+w, y1=y+h}
 end
 
 
 function cbox(x, y, r)
-    return {x1= x-r, y1=y-r, x2=x+r, y2=y+r}
+    return {x0= x-r, y0=y-r, x1=x+r, y1=y+r}
 end
 
 function coll(a, b)
     
-    if a.x1 < b.x2 and
-    a.x2  > b.x1 and
-    a.y1 < b.y2 and
-    a.y2 > b.y1 then
+    if a.x0 < b.x1 and
+    a.x1  > b.x0 and
+    a.y0 < b.y1 and
+    a.y1 > b.y0 then
         return true
     else
         return false
