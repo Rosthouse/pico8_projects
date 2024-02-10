4fa48ed9b0c1f53add5a17212c360529361670cd
Patrick.Joos <Patrick.Joos@ost.ch>
2024-02-10 22:04:13 +0100

    Implemented space Implemented lasers

--- carts/include/particles.lua
+++ carts/include/particles.lua
@@ -1,28 +1,29 @@
 -- Implements a simple finite state machine
-part={
-  c=nil,
-  p={}
+part = {
+  c = nil,
+  p = {}
 }
 
-function part:spw(x,y,d,s,t,c)
+function part:spw(x, y, d, s, t, c, ss, es)
   xd = cos(d)
-  add(self.p,{x=x,y=y,xd=cos(d),yd=sin(d),s=s,t=t,c=c})
+  add(self.p, { x = x, y = y, xd = cos(d), yd = sin(d), s = s, tm = t, t = 0, c = c, ss = ss, es = es })
 end
 
 function part:update()
   for i in all(self.p) do
-    i.t-=1
-    if i.t<0 then
+    i.t += 1
+    if i.t >= i.tm then
       del(self.p, i)
       break
     end
-    i.x+=i.xd*i.s
-    i.y+=i.yd*i.s
+    i.x += i.xd * i.s
+    i.y += i.yd * i.s
   end
 end
 
 function part:draw()
   for i in all(self.p) do
-    circfill(i.x,i.y,1,i.c)
+    local s = lerp(i.ss, i.es, i.t / i.tm)
+    circfill(i.x, i.y, s, i.c)
   end
 end
